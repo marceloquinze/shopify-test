43e3bdba421aaaae3b5ef5fa5a7d4df9ed025963
Marcelo Vieira <marceloquinze@gmail.com>
2025-08-14 12:29:13 -0300

    Fixed dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./templates/**/*.liquid",
+    "./sections/**/*.liquid",
+    "./snippets/**/*.liquid",
+    "./assets/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
